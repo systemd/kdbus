197349d1f0b5f58fa34adfc5cf922ca82be688aa
David Herrmann <dh.herrmann@gmail.com>
2015-06-20 00:53:58 +0200

    kdbus: remove unused code
    kdbus_item_get() and kdbus_item_get_str() are not used anywhere. Drop
    them. They probably got obsoleted during the introduction of kdbus_args.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- ipc/kdbus/item.c
+++ ipc/kdbus/item.c
@@ -260,47 +260,6 @@ int kdbus_items_validate(const struct kdbus_item *items, size_t items_size)
 	return 0;
 }
 
-static struct kdbus_item *kdbus_items_get(const struct kdbus_item *items,
-					  size_t items_size,
-					  unsigned int item_type)
-{
-	const struct kdbus_item *iter, *found = NULL;
-
-	KDBUS_ITEMS_FOREACH(iter, items, items_size) {
-		if (iter->type == item_type) {
-			if (found)
-				return ERR_PTR(-EEXIST);
-			found = iter;
-		}
-	}
-
-	return (struct kdbus_item *)found ? : ERR_PTR(-EBADMSG);
-}
-
-/**
- * kdbus_items_get_str() - get string from a list of items
- * @items:		The items to walk
- * @items_size:		The size of all items
- * @item_type:		The item type to look for
- *
- * This function walks a list of items and searches for items of type
- * @item_type. If it finds exactly one such item, @str_ret will be set to
- * the .str member of the item.
- *
- * Return: the string, if the item was found exactly once, ERR_PTR(-EEXIST)
- * if the item was found more than once, and ERR_PTR(-EBADMSG) if there was
- * no item of the given type.
- */
-const char *kdbus_items_get_str(const struct kdbus_item *items,
-				size_t items_size,
-				unsigned int item_type)
-{
-	const struct kdbus_item *item;
-
-	item = kdbus_items_get(items, items_size, item_type);
-	return IS_ERR(item) ? ERR_CAST(item) : item->str;
-}
-
 /**
  * kdbus_item_set() - Set item content
  * @item:	The item to modify
