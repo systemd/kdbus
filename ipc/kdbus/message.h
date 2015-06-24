156a7e7cb9b90f691024cdf6f49f1a32e7659f8e
David Herrmann <dh.herrmann@gmail.com>
2015-06-24 15:31:10 +0200

    kdbus: drop unused message handling
    Now that we switched to kdbus_staging, the old message importer and
    handling is no longer needed. Drop it.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- ipc/kdbus/message.h
+++ ipc/kdbus/message.h
@@ -17,118 +17,12 @@
 #include <linux/fs.h>
 #include <linux/kref.h>
 #include <uapi/linux/kdbus.h>
-#include "util.h"
-#include "metadata.h"
-
-/**
- * enum kdbus_msg_data_type - Type of kdbus_msg_data payloads
- * @KDBUS_MSG_DATA_VEC:		Data vector provided by user-space
- * @KDBUS_MSG_DATA_MEMFD:	Memfd payload
- */
-enum kdbus_msg_data_type {
-	KDBUS_MSG_DATA_VEC,
-	KDBUS_MSG_DATA_MEMFD,
-};
-
-/**
- * struct kdbus_msg_data - Data payload as stored by messages
- * @type:	Type of payload (KDBUS_MSG_DATA_*)
- * @size:	Size of the described payload
- * @off:	The offset, relative to the vec slice
- * @start:	Offset inside the memfd
- * @file:	Backing file referenced by the memfd
- */
-struct kdbus_msg_data {
-	unsigned int type;
-	u64 size;
-
-	union {
-		struct {
-			u64 off;
-		} vec;
-		struct {
-			u64 start;
-			struct file *file;
-		} memfd;
-	};
-};
-
-/**
- * struct kdbus_kmsg_resources - resources of a message
- * @kref:		Reference counter
- * @dst_name:		Short-cut to msg for faster lookup
- * @fds:		Array of file descriptors to pass
- * @fds_count:		Number of file descriptors to pass
- * @data:		Array of data payloads
- * @vec_count:		Number of VEC entries
- * @memfd_count:	Number of MEMFD entries in @data
- * @data_count:		Sum of @vec_count + @memfd_count
- */
-struct kdbus_msg_resources {
-	struct kref kref;
-	const char *dst_name;
-
-	struct file **fds;
-	unsigned int fds_count;
-
-	struct kdbus_msg_data *data;
-	size_t vec_count;
-	size_t memfd_count;
-	size_t data_count;
-};
-
-struct kdbus_msg_resources *
-kdbus_msg_resources_ref(struct kdbus_msg_resources *r);
-struct kdbus_msg_resources *
-kdbus_msg_resources_unref(struct kdbus_msg_resources *r);
-
-/**
- * struct kdbus_kmsg - internal message handling data
- * @seq:		Domain-global message sequence number
- * @notify:		Short-cut to notify-item for kernel notifications
- * @bloom_filter:	Bloom filter to match message properties
- * @notify_entry:	List of kernel-generated notifications
- * @iov:		Array of iovec, describing the payload to copy
- * @iov_count:		Number of array members in @iov
- * @pool_size:		Overall size of inlined data referenced by @iov
- * @proc_meta:		Appended SCM-like metadata of the sending process
- * @conn_meta:		Appended SCM-like metadata of the sending connection
- * @res:		Message resources
- * @msg:		Message from or to userspace
- */
-struct kdbus_kmsg {
-	u64 seq;
-	struct kdbus_item *notify;
-
-	const struct kdbus_bloom_filter *bloom_filter;
-	struct list_head notify_entry;
-
-	struct iovec *iov;
-	size_t iov_count;
-	u64 pool_size;
-
-	struct kdbus_meta_proc *proc_meta;
-	struct kdbus_meta_conn *conn_meta;
-	struct kdbus_msg_resources *res;
-
-	/* variable size, must be the last member */
-	struct kdbus_msg msg;
-};
 
 struct kdbus_bus;
 struct kdbus_conn;
 struct kdbus_meta_conn;
 struct kdbus_meta_proc;
 struct kdbus_pool_slice;
-
-struct kdbus_kmsg *kdbus_kmsg_new_kernel(struct kdbus_bus *bus,
-					 u64 dst, u64 cookie_timeout,
-					 size_t it_size, size_t it_type);
-struct kdbus_kmsg *kdbus_kmsg_new_from_cmd(struct kdbus_conn *conn,
-					   struct kdbus_cmd_send *cmd_send);
-void kdbus_kmsg_free(struct kdbus_kmsg *kmsg);
-int kdbus_kmsg_collect_metadata(const struct kdbus_kmsg *kmsg,
-				struct kdbus_conn *src, struct kdbus_conn *dst);
 
 /**
  * struct kdbus_gaps - gaps in message to be filled later
