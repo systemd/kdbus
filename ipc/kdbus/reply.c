3c1a8f05a9a1bf8d80cd723910480fa3aef1e360
David Herrmann <dh.herrmann@gmail.com>
2015-09-05 11:35:12 +0200

    kdbus: add missing copyright header
    The reply.c file lacks a copyright header. Copy it from reply.h to make
    sure all our files contain sufficient copyright and license information.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- ipc/kdbus/reply.c
+++ ipc/kdbus/reply.c
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2013-2015 Kay Sievers
+ * Copyright (C) 2013-2015 Greg Kroah-Hartman <gregkh@linuxfoundation.org>
+ * Copyright (C) 2013-2015 Daniel Mack <daniel@zonque.org>
+ * Copyright (C) 2013-2015 David Herrmann <dh.herrmann@gmail.com>
+ * Copyright (C) 2013-2015 Linux Foundation
+ * Copyright (C) 2014-2015 Djalal Harouni <tixxdz@opendz.org>
+ *
+ * kdbus is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ */
+
 #include <linux/init.h>
 #include <linux/mm.h>
 #include <linux/module.h>
